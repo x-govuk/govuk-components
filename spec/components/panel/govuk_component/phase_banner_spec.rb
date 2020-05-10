1f2240195fd1a41d2373ab67c7fcdddf732bf22d
Peter Yates <peter.yates@graphia.co.uk>
2020-05-10 14:15:14 +0100

    Add a pending context for testing block content
    Support for testing the passed-in blocks hasn't landed in the gem yet,
    testing this functionality would require an integration test.
    
    https://github.com/github/view_component/issues/215

--- spec/components/panel/govuk_component/phase_banner_spec.rb
+++ spec/components/panel/govuk_component/phase_banner_spec.rb
@@ -3,30 +3,47 @@ require 'spec_helper'
 RSpec.describe(GovukComponent::PhaseBanner, type: :component) do
   let(:tag) { 'Gamma' }
   let(:text) { 'This is an experimental service – be cautious' }
-  let(:component) { GovukComponent::PhaseBanner.new(tag: tag, text: text) }
   subject { Capybara::Node::Simple.new(render_inline(component).to_html) }
 
   context 'when text is supplied' do
-    specify 'contains a phase banner with the correct tag and body text' do
-      expect(subject).to have_css('div', class: %w(govuk-phase-banner)) do
-        within('govuk-phase-banner__content') do
-          expect(page).to have_css(
-            'strong',
-            class: %w(govuk-tag govuk-phase-banner__content__tag),
-            text: tag
-          )
+    let(:component) { GovukComponent::PhaseBanner.new(tag: tag, text: text) }
 
-          expect(page).to have_css(
-            'span',
-            class: %w(govuk-phase-banner__text),
-            text: text
-          )
+    specify 'contains a phase banner with the correct tag and body text' do
+      expect(subject).to have_css('div', class: %w(govuk-phase-banner)) do |div|
+        expect(div).to have_css('p', class: %w(govuk-phase-banner__content)) do |p|
+          expect(p).to have_css('strong', class: %w(govuk-phase-banner__content__tag), text: tag)
+          expect(p).to have_css('span', class: %w(govuk-phase-banner__text), text: text)
         end
       end
     end
   end
 
-  context 'when a block is supplied' do
-    specify 'contains a phase banner with the correct tag and body html'
+  # This approach won't work until support for passing blocks to components in
+  # tests is added, see https://github.com/github/view_component/issues/215
+  xcontext 'when a block is supplied' do
+    let(:warning) { 'This is an Alpha project, ignore everything' }
+
+    let(:content) do
+      capture do
+        content_tag('section') do
+          concat(tag.strong(warning))
+        end
+      end
+    end
+
+    let(:component) do
+      GovukComponent::PhaseBanner.new(tag: tag) { content }
+    end
+
+    specify 'contains a phase banner with the correct tag and body html' do
+      expect(subject).to have_css('div', class: %w(govuk-phase-banner)) do |div|
+        expect(div).to have_css('p', class: %w(govuk-phase-banner__content)) do |p|
+          expect(p).to have_css('strong', class: %w(govuk-phase-banner__content__tag), text: tag)
+          expect(p).to have_css('span', class: %w(govuk-phase-banner__text)) do |span|
+            expect(span).to have_css('strong', text: warning)
+          end
+        end
+      end
+    end
   end
 end
