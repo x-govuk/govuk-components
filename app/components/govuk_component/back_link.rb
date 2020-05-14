d83f6f97e0ca6d6bdc7013c1caca4746109e0595
Alistair Laing <aliuk2012@users.noreply.github.com>
2020-05-14 22:26:51 +0100

    Back link should accept custom html attributes
    Inital I thought of using native html but this proved to be troublesome
    when it came to introducing custom html attributes. I did think about just
    a hash but again this was a bit fiddly.
    
    The only difference now between this component and govuk-frontend
    design system version is that their back link accepts custom html
    to be passed as link text. I'm not sure I agree with this because
    theres risk that user could try pass it a link which would be invalid
    to have an link inside a link. I think for the moment we can afford
    not to support it until someone requests it.

--- app/components/govuk_component/back_link.rb
+++ app/components/govuk_component/back_link.rb
@@ -1,9 +1,13 @@
 class GovukComponent::BackLink < ViewComponent::Base
-  attr_accessor :text, :href
+  attr_accessor :text, :href, :options
 
-  def initialize(text:, href:, classes: nil)
+  def initialize(text:, href:, classes: nil, attributes: nil)
     @text = text
     @href = href
-    @classes = classes
+    @classes = "govuk-back-link"
+    @classes << " #{classes}" if classes.present?
+
+    @options = { class: @classes }
+    @options.merge!(attributes) if attributes.present?
   end
 end
--- spec/components/govuk_component/back_link_spec.rb
+++ spec/components/govuk_component/back_link_spec.rb
@@ -21,7 +21,38 @@ RSpec.describe(GovukComponent::BackLink, type: :component) do
     subject { Capybara::Node::Simple.new(render_inline(component).to_html) }
 
     specify 'contains a link styled as a back link with the correct href' do
-      expect(subject).to have_css('a', text: text, class: %w(govuk-back-link app-custom--class app-custom--class-2))
+      expect(subject).to have_css('a',
+                                  text: text,
+                                  class: %w(govuk-back-link app-custom--class app-custom--class-2))
     end
   end
+
+  context 'accepts custom HTML attributes' do
+    let(:component) {
+      GovukComponent::BackLink.new(text: text, href: href,
+                                   attributes: { data_html5: 'Attributes',
+                                                 id: 'my-back-link'})
+    }
+    subject { Capybara::Node::Simple.new(render_inline(component).to_html) }
+
+    specify 'contains a link styled as a back link with custom html attributes' do
+      expect(subject).to have_css('a', text: text, id: 'my-back-link')
+    end
+  end
+
+  context 'accepts custom css classes and multiple HTML attributes' do
+    let(:component) {
+      GovukComponent::BackLink.new(text: text, href: href, classes: 'app-style',
+                                   attributes: { data_html5: 'Attributes',
+                                                 id: 'my-back-link',
+                                                 data: { tag: 'element'} })
+    }
+    subject { Capybara::Node::Simple.new(render_inline(component).to_html) }
+
+    specify 'contains a link styled as a back link with custom html attributes' do
+      expect(subject).to have_css('a#my-back-link.app-style[data-tag="element"]',
+                                  text: text)
+    end
+  end
+
 end
