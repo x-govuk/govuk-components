ca21514b052edc0ae88937192e5f3f80e9b6210f
Peter Yates <peter.yates@graphia.co.uk>
2020-08-23 18:38:43 +0100

    Use a glob to load partials instead of hardcoded

--- spec/dummy/app/controllers/demos_controller.rb
+++ spec/dummy/app/controllers/demos_controller.rb
@@ -1,4 +1,7 @@
 class DemosController < ApplicationController
   def show
+    @partials = Dir
+      .glob(Rails.root.join('app', 'views', 'demos', 'examples', '*.html.erb'))
+      .map { |f| File.basename(f)[%r{_(?<partial>.*)\.html.erb}, %(partial)] }
   end
 end
