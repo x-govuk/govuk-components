3bd9ff55799bffa6c60095a6288741e00a195b4a
Peter Yates <peter.yates@graphia.co.uk>
2020-08-23 19:02:55 +0100

    Move file listing to the view
    It doesn't work when run as part of the rake task when it's in the
    controller. As it's _only_ documentation let's just put it where it
    works for now

--- spec/dummy/app/controllers/demos_controller.rb
+++ spec/dummy/app/controllers/demos_controller.rb
@@ -1,8 +1,4 @@
 class DemosController < ApplicationController
   def show
-    @partials = Dir
-      .glob(Rails.root.join('app', 'views', 'demos', 'examples', '*.html.erb'))
-      .map { |f| File.basename(f)[%r{_(?<partial>.*)\.html.erb}, %(partial)] }
-      .sort
   end
 end
