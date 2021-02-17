57faf242c7729aa37f5f75c8981de0ae4438a0a0
Connor McQuillan <connor.mcquillan17@gmail.com>
2021-02-17 10:40:37 +0000

    Add GovukComponent::CookieBanner

---
+++ app/components/govuk_component/cookie_banner.rb
@@ -0,0 +1,17 @@
+class GovukComponent::CookieBanner < GovukComponent::Base
+  with_content_areas :body, :actions
+
+  attr_accessor :title
+
+  def initialize(title:, classes: [], html_attributes: {})
+    super(classes: classes, html_attributes: html_attributes)
+
+    @title = title
+  end
+
+private
+
+  def default_classes
+    %w(govuk-cookie-banner)
+  end
+end
