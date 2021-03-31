ee5cec1b792b7adc6ff6dc8860d47a1311f0ed2b
Peter Yates <peter.yates@graphia.co.uk>
2021-03-31 09:36:42 +0100

    Improvements to the panel component
    The panel wasn't very flexible and required a title and body. Now it
    supports rendering without a title or body and can accept a block of
    HTML instead of plain body text.
    
    When no arguments or block are supplied nothing will be rendered.
    
    Fixes #135

--- app/components/govuk_component/panel.rb
+++ app/components/govuk_component/panel.rb
@@ -1,7 +1,7 @@
 class GovukComponent::Panel < GovukComponent::Base
   attr_accessor :title, :body
 
-  def initialize(title:, body:, classes: [], html_attributes: {})
+  def initialize(title: nil, body: nil, classes: [], html_attributes: {})
     super(classes: classes, html_attributes: html_attributes)
 
     @title = title
@@ -12,5 +12,17 @@ private
 
   def default_classes
     %w(govuk-panel govuk-panel--confirmation)
+  end
+
+  def display_title?
+    @title.present?
+  end
+
+  def display_body?
+    (@body || content).present?
+  end
+
+  def render?
+    display_title? || display_body?
   end
 end
