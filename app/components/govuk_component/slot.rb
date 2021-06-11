a1cd2125ee0002e637f5fcadc3034e1b78932f6d
Peter Yates <peter.yates@graphia.co.uk>
2021-06-11 22:36:25 +0100

    Remove no-longer-needed GovukComponent::Slot

--- app/components/govuk_component/slot.rb
+++
@@ -1,9 +0,0 @@
-class GovukComponent::Slot < ViewComponent::Slot
-  include GovukComponent::Traits::CustomClasses
-  include GovukComponent::Traits::CustomHtmlAttributes
-
-  def initialize(classes: [], html_attributes: {})
-    @classes         = parse_classes(classes)
-    @html_attributes = html_attributes
-  end
-end
