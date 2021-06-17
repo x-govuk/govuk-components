dd550b6fd87754ee10ec77f6f39188eafce67a82
Peter Yates <peter.yates@graphia.co.uk>
2021-06-17 10:18:43 +0100

    Rework the link specs to use RSpec HTML Matchers

--- spec/helpers/govuk_link_helper_spec.rb
+++ spec/helpers/govuk_link_helper_spec.rb
@@ -5,205 +5,180 @@ RSpec.describe(GovukLinkHelper, type: 'helper') do
   include ActionView::Context
 
   let(:text) { 'Menu' }
-  subject { Capybara::Node::Simple.new(component) }
+  let(:url) { '/stuff/menu/' }
+  let(:args) { [text, url] }
+  let(:kwargs) { {} }
 
   describe '#govuk_link_to' do
-    let(:url) { '/stuff/menu/' }
-    let(:component) { govuk_link_to(text, url) }
-    it { is_expected.to(have_link(text, href: url, class: 'govuk-link')) }
+    subject { govuk_link_to(*args, **kwargs) }
+    it { is_expected.to have_tag("a", with: { href: url, class: 'govuk-link' }) }
 
-    context 'when additional classes are passed in' do
-      let(:custom_class) { 'yellow' }
-      let(:component) { govuk_link_to(text, url, class: custom_class) }
+    describe "custom classes" do
+      context 'when additional classes are passed in as strings' do
+        let(:custom_class) { 'yellow' }
+        let(:kwargs) { { class: custom_class } }
 
-      specify 'has the custom classes' do
-        expect(subject).to(have_link(text, href: url, class: ['govuk-link', custom_class]))
+        specify 'has the custom classes' do
+          expect(subject).to have_tag("a", with: { href: url, class: ['govuk-link', custom_class] })
+        end
       end
-    end
 
-    context 'when additional classes are passed in as arrays' do
-      let(:custom_class) { %w(yellow) }
-      let(:component) { govuk_link_to(text, url, class: custom_class) }
+      context 'when additional classes are passed in as arrays' do
+        let(:custom_class) { %w(yellow) }
+        let(:kwargs) { { class: custom_class } }
 
-      specify 'has the custom classes' do
-        expect(subject).to(have_link(text, href: url, class: ['govuk-link', custom_class].flatten))
+        specify 'has the custom classes' do
+          expect(subject).to have_tag("a", with: { href: url, class: custom_class.append("govuk-link") })
+        end
       end
     end
 
     context 'when provided with a block' do
       let(:link_text) { 'Onwards!' }
       let(:link_html) { tag.span(link_text) }
-      let(:component) { govuk_link_to(url) { link_html } }
+      subject { govuk_link_to(url) { link_html } }
 
-      specify 'should render a link containing the block content' do
-        expect(subject).to have_css('a > span', text: link_text)
+      specify 'renders a link containing the block content' do
+        expect(subject).to have_tag('a') do
+          with_tag('span', text: link_text)
+        end
       end
     end
 
-    describe 'generating a button-style link' do
-      let(:component) { govuk_link_to(text, url, button: true) }
+    {
+      button: %w(govuk-button),
+      no_visited_state: %w(govuk-link govuk-link--no-visited-state),
+      muted: %w(govuk-link govuk-link--muted),
+      colour: %w(govuk-link govuk-link--colour),
+      inverse: %w(govuk-link govuk-link--inverse),
+      no_underline: %w(govuk-link govuk-link--no-underline),
+    }.each do |style, css_class|
+      describe "generating a #{style}-style link with '#{style}: true'" do
+        let(:kwargs) { { style => true } }
 
-      specify 'has the button class' do
-        expect(subject).to(have_link(text, href: url, class: 'govuk-button'))
-      end
-    end
-
-    describe 'generating a no_visited_state link' do
-      let(:component) { govuk_link_to(text, url, no_visited_state: true) }
-
-      specify 'has the no-visted-state class' do
-        expect(subject).to(have_link(text, href: url, class: 'govuk-link govuk-link--no-visited-state'))
-      end
-    end
-
-    describe 'generating a muted link' do
-      let(:component) { govuk_link_to(text, url, muted: true) }
-
-      specify 'has the muted class' do
-        expect(subject).to(have_link(text, href: url, class: 'govuk-link govuk-link--muted'))
-      end
-    end
-
-    describe 'generating a coloured link' do
-      let(:component) { govuk_link_to(text, url, colour: true) }
-
-      specify 'has the coloured class' do
-        expect(subject).to(have_link(text, href: url, class: 'govuk-link govuk-link--colour'))
-      end
-    end
-
-    describe 'generating a inverse link' do
-      let(:component) { govuk_link_to(text, url, inverse: true) }
-
-      specify 'has the inverse class' do
-        expect(subject).to(have_link(text, href: url, class: 'govuk-link govuk-link--inverse'))
-      end
-    end
-
-    describe 'generating a no-underline link' do
-      let(:component) { govuk_link_to(text, url, no_underline: true) }
-
-      specify 'has the no-underline class' do
-        expect(subject).to(have_link(text, href: url, class: 'govuk-link govuk-link--no-underline'))
+        specify "link has the class: #{css_class}" do
+          expect(subject).to have_tag('a', text: text, with: { href: url, class: css_class })
+        end
       end
     end
   end
 
   describe '#govuk_mail_to' do
     let(:email_address) { %(test@something.org) }
-    let(:target) { %(mailto:) + email_address }
-    let(:component) { govuk_mail_to(email_address, text) }
-    it { is_expected.to(have_link(text, href: target, class: 'govuk-link')) }
+    let(:email_address_with_mailto_prefix) { %(mailto:) + email_address }
+    let(:args) { [email_address, text] }
+    subject { govuk_mail_to(*args, **kwargs) }
 
-    context 'when additional classes are passed in' do
-      let(:custom_class) { 'yellow' }
-      let(:component) { govuk_mail_to(email_address, text, class: custom_class) }
+    it { is_expected.to have_tag("a", with: { href: email_address_with_mailto_prefix, class: 'govuk-link' }) }
 
-      specify 'has the custom classes' do
-        expect(subject).to(have_link(text, href: target, class: ['govuk-link', custom_class]))
+    describe "custom classes" do
+      context 'when additional classes are passed in as strings' do
+        let(:custom_class) { 'yellow' }
+        let(:kwargs) { { class: custom_class } }
+
+        specify 'has the custom classes' do
+          expect(subject).to have_tag("a", with: { href: email_address_with_mailto_prefix, class: ['govuk-link', custom_class] })
+        end
       end
-    end
 
-    context 'when additional classes are passed in as arrays' do
-      let(:custom_class) { %w(yellow) }
-      let(:component) { govuk_mail_to(email_address, text, class: custom_class) }
+      context 'when additional classes are passed in as arrays' do
+        let(:custom_class) { %w(yellow) }
+        let(:kwargs) { { class: custom_class } }
 
-      specify 'has the custom classes' do
-        expect(subject).to(have_link(text, href: target, class: ['govuk-link', custom_class].flatten))
+        specify 'has the custom classes' do
+          expect(subject).to have_tag("a", with: { href: email_address_with_mailto_prefix, class: custom_class.append("govuk-link") })
+        end
       end
     end
 
     context 'when provided with a block' do
-      let(:link_text) { 'Contact us!' }
+      let(:link_text) { 'Onwards!' }
       let(:link_html) { tag.span(link_text) }
-      let(:component) { govuk_mail_to(email_address) { link_html } }
+      subject { govuk_link_to(url) { link_html } }
 
-      specify 'should render a link containing the block content' do
-        expect(subject).to have_css('a > span', text: link_text)
+      specify 'renders a link containing the block content' do
+        expect(subject).to have_tag('a') do
+          with_tag('span', text: link_text)
+        end
       end
     end
 
-    describe 'generating a button-style link' do
-      let(:component) { govuk_link_to(text, email_address, button: true) }
+    {
+      button: %w(govuk-button),
+      no_visited_state: %w(govuk-link govuk-link--no-visited-state),
+      muted: %w(govuk-link govuk-link--muted),
+      colour: %w(govuk-link govuk-link--colour),
+      inverse: %w(govuk-link govuk-link--inverse),
+      no_underline: %w(govuk-link govuk-link--no-underline),
+    }.each do |style, css_class|
+      describe "generating a #{style}-style link with '#{style}: true'" do
+        let(:kwargs) { { style => true } }
 
-      specify 'has the button class' do
-        expect(subject).to(have_link(text, class: 'govuk-button'))
-      end
-    end
-
-    describe 'generating a no_visited_state link' do
-      let(:component) { govuk_link_to(text, email_address, no_visited_state: true) }
-
-      specify 'has the no-visted-state class' do
-        expect(subject).to(have_link(text, class: 'govuk-link govuk-link--no-visited-state'))
-      end
-    end
-
-    describe 'generating a muted link' do
-      let(:component) { govuk_link_to(text, email_address, muted: true) }
-
-      specify 'has the muted class' do
-        expect(subject).to(have_link(text, class: 'govuk-link govuk-link--muted'))
-      end
-    end
-
-    describe 'generating a coloured link' do
-      let(:component) { govuk_link_to(text, email_address, colour: true) }
-
-      specify 'has the coloured class' do
-        expect(subject).to(have_link(text, class: 'govuk-link govuk-link--colour'))
-      end
-    end
-
-    describe 'generating a inverse link' do
-      let(:component) { govuk_link_to(text, email_address, inverse: true) }
-
-      specify 'has the inverse class' do
-        expect(subject).to(have_link(text, class: 'govuk-link govuk-link--inverse'))
-      end
-    end
-
-    describe 'generating a no-underline link' do
-      let(:component) { govuk_link_to(text, email_address, no_underline: true) }
-
-      specify 'has the inverse class' do
-        expect(subject).to(have_link(text, class: 'govuk-link govuk-link--no-underline'))
+        specify "link has the class: #{css_class}" do
+          expect(subject).to have_tag('a', text: text, with: { href: email_address_with_mailto_prefix, class: css_class })
+        end
       end
     end
   end
 
   describe '#govuk_button_to' do
+    subject { govuk_button_to(*args, **kwargs) }
     let(:url) { '/stuff/menu/' }
-    let(:component) { govuk_button_to(text, url) }
-    it { is_expected.to(have_button(text, class: 'govuk-button')) }
 
     specify 'has form with correct url containing submit input with supplied text' do
-      expect(subject).to have_css(%(form)) do |form|
-        expect(form['action']).to eql(url)
-        expect(form).to have_button(text, class: 'govuk-button')
+      # expect(subject).to have_css(%(form)) do |form|
+      #   expect(form['action']).to eql(url)
+      #   expect(form).to have_button(text, class: 'govuk-button')
+      # end
+      expect(subject).to have_tag('form', with: { action: url }) do
+        with_tag('input', with: { class: 'govuk-button', value: text })
       end
     end
 
-    context 'when additional classes are passed in' do
-      let(:custom_class) { 'yellow' }
-      let(:component) { govuk_button_to(text, url, class: custom_class) }
+    describe "custom classes" do
+      context 'when additional classes are passed in as strings' do
+        let(:custom_class) { 'yellow' }
+        let(:kwargs) { { class: custom_class } }
 
-      specify 'has the custom classes' do
-        expect(subject).to(have_button(text, class: ['govuk-button', custom_class].flatten))
+        specify 'has the custom classes' do
+          expect(subject).to have_tag('form', with: { action: url }) do
+            with_tag("input", with: { class: ['govuk-button', custom_class] })
+          end
+        end
       end
 
-      specify 'has the default class' do
-        expect(subject).to(have_button(text, class: 'govuk-button'))
+      context 'when additional classes are passed in as arrays' do
+        let(:custom_class) { %w(yellow) }
+        let(:kwargs) { { class: custom_class } }
+
+        specify 'has the custom classes' do
+          expect(subject).to have_tag('form', with: { action: url }) do
+            with_tag("input", with: { class: ['govuk-button', custom_class] })
+          end
+        end
       end
     end
 
-    context 'when additional classes are passed in as arrays' do
-      let(:custom_class) { %w(yellow) }
-      let(:component) { govuk_button_to(text, url, class: custom_class) }
+    # context 'when additional classes are passed in' do
+    #   let(:custom_class) { 'yellow' }
+    #   let(:component) { govuk_button_to(text, url, class: custom_class) }
 
-      specify 'has the custom classes' do
-        expect(subject).to(have_button(text, class: custom_class))
-      end
-    end
+    #   specify 'has the custom classes' do
+    #     expect(subject).to(have_button(text, class: ['govuk-button', custom_class].flatten))
+    #   end
+
+    #   specify 'has the default class' do
+    #     expect(subject).to(have_button(text, class: 'govuk-button'))
+    #   end
+    # end
+
+    # context 'when additional classes are passed in as arrays' do
+    #   let(:custom_class) { %w(yellow) }
+    #   let(:component) { govuk_button_to(text, url, class: custom_class) }
+
+    #   specify 'has the custom classes' do
+    #     expect(subject).to(have_button(text, class: custom_class))
+    #   end
+    # end
   end
 end
