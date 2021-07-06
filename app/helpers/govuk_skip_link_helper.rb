e420f1f26ea6bbe84b89a8d52f2b3509be41b801
Peter Yates <peter.yates@graphia.co.uk>
2021-07-06 17:07:46 +0100

    Add skip link support for custom HTML and classes

--- app/helpers/govuk_skip_link_helper.rb
+++ app/helpers/govuk_skip_link_helper.rb
@@ -1,6 +1,11 @@
 module GovukSkipLinkHelper
-  def govuk_skip_link(text: 'Skip to main content', href: '#main-content')
-    link_to text, href, class: 'govuk-skip-link'
+  def govuk_skip_link(text: 'Skip to main content', href: '#main-content', classes: [], **html_attributes, &block)
+    link_classes = Array.wrap(classes).append('govuk-skip-link')
+
+    return link_to(href, class: link_classes, **html_attributes, &block) if block_given?
+
+    link_to(text, href, class: link_classes, **html_attributes)
   end
 end
+
 ActiveSupport.on_load(:action_view) { include GovukSkipLinkHelper }
