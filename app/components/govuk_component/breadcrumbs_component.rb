f39384a5a2f47f50faf6e7f62cf5a369d93ddd7d
Peter Yates <peter.yates@graphia.co.uk>
2021-07-14 09:18:47 +0100

    Allow breadcrumbs to take an array of content
    This should allow extra flexibility when creating lists of breadcrumbs.
    The contents of the breadcrumb link array will be entirely customisable
    by the developer, including adding custom HTML attributes to each list
    item.
    
    A new #govuk_breadcrumb_link_to will be added shortly that can be used
    in conjunction with the BreadcrumbsComponent

--- app/components/govuk_component/breadcrumbs_component.rb
+++ app/components/govuk_component/breadcrumbs_component.rb
@@ -4,7 +4,7 @@ class GovukComponent::BreadcrumbsComponent < GovukComponent::Base
   def initialize(breadcrumbs:, hide_in_print: false, collapse_on_mobile: false, classes: [], html_attributes: {})
     super(classes: classes, html_attributes: html_attributes)
 
-    @breadcrumbs        = breadcrumbs
+    @breadcrumbs        = build_list(breadcrumbs)
     @hide_in_print      = hide_in_print
     @collapse_on_mobile = collapse_on_mobile
   end
@@ -16,5 +16,28 @@ private
       classes << "govuk-!-display-none-print" if hide_in_print
       classes << "govuk-breadcrumbs--collapse-on-mobile" if collapse_on_mobile
     end
+  end
+
+  def build_list(breadcrumbs)
+    case breadcrumbs
+    when Array
+      breadcrumbs.map { |item| build_list_item(item) }
+    when Hash
+      breadcrumbs.map { |text, link| build_list_item(text, link) }
+    else
+      fail(ArgumentError, "breadcrumb must be an array or hash")
+    end
+  end
+
+  def build_list_item(text, link = nil)
+    if link.present?
+      list_item { link_to(text, link, class: "govuk-breadcrumbs__link") }
+    else
+      list_item(aria: { current: "page" }) { text }
+    end
+  end
+
+  def list_item(html_attributes = {}, &block)
+    tag.li(class: "govuk-breadcrumbs__list-item", **html_attributes, &block)
   end
 end
