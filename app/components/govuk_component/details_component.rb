79d7e47174c1cc553c81563ba37467d47651f7a9
Peter Yates <peter.yates@graphia.co.uk>
2021-07-18 19:31:43 +0100

    Add custom id and open status params to details

--- app/components/govuk_component/details_component.rb
+++ app/components/govuk_component/details_component.rb
@@ -1,17 +1,19 @@
 class GovukComponent::DetailsComponent < GovukComponent::Base
-  attr_reader :summary_text, :text
+  attr_reader :summary_text, :text, :id, :open
 
   renders_one :summary_html
 
-  def initialize(summary_text:, text: nil, classes: [], html_attributes: {})
+  def initialize(summary_text:, text: nil, classes: [], id: nil, open: nil, html_attributes: {})
     super(classes: classes, html_attributes: html_attributes)
 
     @summary_text = summary_text
     @text         = text
+    @id           = id
+    @open         = open
   end
 
   def call
-    tag.details(class: classes, data: { module: "govuk-details" }, **html_attributes) do
+    tag.details(class: classes, data: { module: "govuk-details" }, id: id, open: open, **html_attributes) do
       safe_join([summary, description])
     end
   end
