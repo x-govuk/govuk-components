308c564fa6aef6ad9931d2e879a41a7dd44ee66c
Peter Yates <peter.yates@graphia.co.uk>
2021-08-29 11:51:02 +0100

    Mount lookback at the dummy application's root

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'demos#show'
   mount Govuk::Components::Engine => "/govuk-components"
-  mount Lookbook::Engine, at: "/lookbook"
+  mount Lookbook::Engine, at: "/"
 end
