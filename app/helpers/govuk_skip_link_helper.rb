b6a0f47e9b4226bbae1a0656fba9e1f1bcd5c08b
Peter Yates <peter.yates@graphia.co.uk>
2022-01-06 16:40:04 +0000

    Ensure data-module='govuk-skip-link' present

--- app/helpers/govuk_skip_link_helper.rb
+++ app/helpers/govuk_skip_link_helper.rb
@@ -2,9 +2,11 @@ module GovukSkipLinkHelper
   def govuk_skip_link(text: 'Skip to main content', href: '#main-content', classes: [], **html_attributes, &block)
     link_classes = Array.wrap(classes).append('govuk-skip-link')
 
-    return link_to(href, class: link_classes, **html_attributes, &block) if block_given?
+    html_attributes_with_data_module = { data: { module: "govuk-skip-link" } }.deep_merge(html_attributes)
 
-    link_to(text, href, class: link_classes, **html_attributes)
+    return link_to(href, class: link_classes, **html_attributes_with_data_module, &block) if block_given?
+
+    link_to(text, href, class: link_classes, **html_attributes_with_data_module)
   end
 end
 
