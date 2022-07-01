57c0b84c087c7958441e398acc52aabbf0182898
Thomas Leese <thomas@leese.io>
2022-07-01 05:14:45 +0100

    Add example use of start button as button
    This adds an example in the guide of using the start button component,
    but as a form with a submit button.

--- guide/lib/examples/start_button_helpers.rb
+++ guide/lib/examples/start_button_helpers.rb
@@ -5,5 +5,11 @@ module Examples
         = govuk_start_button(text: "Start now", href: "#")
       START_BUTTON
     end
+
+    def start_button_as_button
+      <<~START_BUTTON
+        = govuk_start_button(text: "Start now", href: "#", as_button: true)
+      START_BUTTON
+    end
   end
 end
