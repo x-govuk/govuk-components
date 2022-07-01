61bc78bd217dab0890bc78860033fe61da963803
Thomas Leese <thomas@leese.io>
2022-07-01 05:10:20 +0100

    Simplify custom HTML attributes shared example
    Rather than enforcing the check on the first element, we instead make
    sure an element exists. This better handles the situation where a
    component might be wrapped.

--- spec/components/shared/a_component_that_accepts_custom_html_attributes.rb
+++ spec/components/shared/a_component_that_accepts_custom_html_attributes.rb
@@ -11,11 +11,9 @@ shared_examples 'a component that accepts custom HTML attributes' do
   context 'classes' do
     let(:custom_class) { 'red-and-yellow-stripes' }
     let(:updated_kwargs) { kwargs.deep_merge({ html_attributes: { class: Array.wrap(custom_class) } }) }
-    let(:actual_classes) { html.at('/*[1]').attr('class').split }
 
     specify 'the custom class is merged with the default ones' do
-      expect(actual_classes).to include(custom_class)
-      expect(actual_classes).to include(component_css_class)
+      expect(rendered_content).to have_tag('*', with: { class: [custom_class, component_css_class] })
     end
   end
 end
