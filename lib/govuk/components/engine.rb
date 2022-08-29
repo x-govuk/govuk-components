4c964f77d4baeea75c7331547bfdf8147e5abd44
Peter Yates <peter.yates@graphia.co.uk>
2022-08-29 20:29:32 +0100

    Add rdoc entries for configuration items
    Currently just listing the default. I think for the most part the names
    are descriptive enough to know what each one will do.

--- lib/govuk/components/engine.rb
+++ lib/govuk/components/engine.rb
@@ -28,6 +28,40 @@ module Govuk
       end
     end
 
+    # @!group Defaults
+    #
+    # Default components configuration
+    #
+    # +:default_back_link_text+ Default text for the back link, defaults to +Back+
+    # +:default_breadcrumbs_collapse_on_mobile+ false
+    # +:default_breadcrumbs_hide_in_print+ false
+    # +:default_cookie_banner_aria_label+ "Cookie banner"
+    # +:default_cookie_banner_hide_in_print+ true
+    # +:default_header_navigation_label+ 'Navigation menu'
+    # +:default_header_menu_button_label+ 'Show or hide navigation menu'
+    # +:default_header_logotype+ 'GOV.UK'
+    # +:default_header_homepage_url+ '/'
+    # +:default_header_service_name+ nil
+    # +:default_header_service_url+ '/'
+    # +:default_footer_meta_text+ nil
+    # +:default_footer_copyright_text+ '© Crown copyright'
+    # +:default_footer_copyright_url+ "https://www.nationalarchives.gov.uk/information-management/re-using-public-sector-information/uk-government-licensing-framework/crown-copyright/"
+    # +:default_pagination_landmark_label+ "results"
+    # +:default_pagination_next_text+ Default 'next' text for pagination. An +Array+ where the first item is visible and the second visually hidden. Defaults to ["Next", "page"]
+    # +:default_pagination_previous_text+ Default 'previous' text for pagination. An +Array+ where the first item is visible and the second visually hidden. Defaults to ["Previous", "page"]
+    # +:default_phase_banner_tag+ nil
+    # +:default_phase_banner_text+ nil
+    # +:default_section_break_visible+ false
+    # +:default_section_break_size+ Size of the section break, possible values: +m+, +l+ and +xl+. Defaults to nil.
+    # +:default_tag_colour+ the default colour for tags, possible values: +grey+, +green+, +turquoise+, +blue+, +red+, +purple+, +pink+, +orange+, +yellow+. Defaults to +nil+
+    # +:default_start_button_as_button+ false
+    # +:default_summary_list_borders+ true
+    # +:default_notification_banner_title_id+ "govuk-notification-banner-title"
+    # +:default_notification_disable_auto_focus+ nil
+    # +:default_notification_title_heading_level+ 2
+    # +:default_notification_title_success+ false
+    # +:default_warning_text_icon_fallback_text+ "Warning"
+    # +:default_warning_text_icon+ "!"
     DEFAULTS = {
       default_back_link_text: 'Back',
       default_breadcrumbs_collapse_on_mobile: false,
