1bc6aa8938d3d4e4a9686b180ddd9a73b25f613c
Peter Yates <peter.yates@graphia.co.uk>
2023-02-02 18:26:55 +0000

    Remove no longer needed deep merge for classes

--- app/components/govuk_component/base.rb
+++ app/components/govuk_component/base.rb
@@ -5,21 +5,8 @@ class GovukComponent::Base < ViewComponent::Base
 
   delegate :config, to: Govuk::Components
 
-  def initialize(classes:, html_attributes:)
-    if classes.nil?
-      Rails.logger.warn("classes is nil, if no custom classes are needed omit the param")
-
-      classes = []
-    end
-    # FIXME: remove first merge when we deprecate classes
-    #
-    # This step only needs to be here while we still accept classes:, now
-    # we're using html_attributes_utils we can start to move towards
-    # supporting html_attributes: { class: 'xyz' } over taking them
-    # separately
-
+  def initialize(html_attributes:)
     @html_attributes = default_attributes
-      .deep_merge_html_attributes({ class: classes })
       .deep_merge_html_attributes(html_attributes)
       .deep_tidy_html_attributes
 
