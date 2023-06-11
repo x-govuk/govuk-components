e925f0a3293859913b00485d701b994b23a5b1c5
Peter Yates <peter.yates@graphia.co.uk>
2023-06-11 10:50:04 +0100

    Replace Nanoc's LinkTo with Rails' UrlHelper
    Nanoc's link helpers expect classes to be provided in a string but now
    we're using arrays internally throughout so it makes sense to use the
    Rails version.

--- guide/lib/helpers.rb
+++ guide/lib/helpers.rb
@@ -16,8 +16,8 @@ require 'pagy'
 
 Dir.glob(File.join('./lib', '**', '*.rb')).sort.each { |f| require f }
 
+use_helper ActionView::Helpers::UrlHelper
 use_helper Nanoc::Helpers::Rendering
-use_helper Nanoc::Helpers::LinkTo
 use_helper Nanoc::Helpers::XMLSitemap
 use_helper Helpers::LinkHelpers
 use_helper Helpers::TitleAnchorHelpers
