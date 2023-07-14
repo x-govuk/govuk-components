08b4d0df5a3aa3182765b0b283cb97f13c4af4f9
Theodor Vararu <theo@vararu.org>
2023-07-14 15:03:59 +0100

    Brand: Breadcrumbs component

--- app/components/govuk_component/breadcrumbs_component.rb
+++ app/components/govuk_component/breadcrumbs_component.rb
@@ -21,10 +21,10 @@ private
   def default_attributes
     {
       class: class_names(
-        "govuk-breadcrumbs",
-        "govuk-!-display-none-print" => hide_in_print,
-        "govuk-breadcrumbs--collapse-on-mobile" => collapse_on_mobile,
-        "govuk-breadcrumbs--inverse" => inverse
+        "#{brand}-breadcrumbs",
+        "#{brand}-!-display-none-print" => hide_in_print,
+        "#{brand}-breadcrumbs--collapse-on-mobile" => collapse_on_mobile,
+        "#{brand}-breadcrumbs--inverse" => inverse
       ).split
     }
   end
@@ -42,13 +42,13 @@ private
 
   def build_list_item(text, link = nil)
     if link.present?
-      list_item { link_to(text, link, class: "govuk-breadcrumbs__link") }
+      list_item { link_to(text, link, class: "#{brand}-breadcrumbs__link") }
     else
       list_item(aria: { current: "page" }) { text.to_s }
     end
   end
 
   def list_item(html_attributes = {}, &block)
-    tag.li(class: "govuk-breadcrumbs__list-item", **html_attributes, &block)
+    tag.li(class: "#{brand}-breadcrumbs__list-item", **html_attributes, &block)
   end
 end
