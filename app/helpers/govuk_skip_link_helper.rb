331095f92c42a0b2b215afa05e59740ef6ead354
Theodor Vararu <theo@vararu.org>
2023-07-14 19:08:28 +0100

    Brand: Skip, Exit, Back to top links

--- app/helpers/govuk_skip_link_helper.rb
+++ app/helpers/govuk_skip_link_helper.rb
@@ -1,8 +1,8 @@
 module GovukSkipLinkHelper
   def govuk_skip_link(text: 'Skip to main content', href: '#main-content', classes: [], **html_attributes, &block)
-    link_classes = Array.wrap(classes).append('govuk-skip-link')
+    link_classes = Array.wrap(classes).append("#{ brand }-skip-link")
 
-    html_attributes_with_data_module = { data: { module: "govuk-skip-link" } }.deep_merge(html_attributes)
+    html_attributes_with_data_module = { data: { module: "#{ brand }-skip-link" } }.deep_merge(html_attributes)
 
     return link_to(href, class: link_classes, **html_attributes_with_data_module, &block) if block_given?
 
