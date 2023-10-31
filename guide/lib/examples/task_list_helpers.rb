1169bcbd1ed8f5198a7f8fc8ecca4f60041ecdbd
Frankie Roberto <frankie@frankieroberto.com>
2023-10-31 09:29:46 +0000

    Update the docs a bit

--- guide/lib/examples/task_list_helpers.rb
+++ guide/lib/examples/task_list_helpers.rb
@@ -2,32 +2,40 @@ module Examples
   module TaskListHelpers
     def default_task_list
       <<~SNIPPET
-        = govuk_task_list do |task_list|
+        = govuk_task_list(id_prefix: "project-tasks") do |task_list|
           - task_list.with_item(title: "Contact details", href: '#', status: "Completed")
           - task_list.with_item(title: "Project details", href: '#', status: "Completed")
           - task_list.with_item(title: "Funding",         href: '#', status: govuk_tag(text: "Incomplete", colour: "blue"))
       SNIPPET
     end
 
+    def task_list_with_cannot_start_yet
+      <<~SNIPPET
+        = govuk_task_list(id_prefix: "project-tasks") do |task_list|
+          - task_list.with_item(title: "Contact details", href: '#', status: "Completed")
+          - task_list.with_item(title: "Project details", href: '#', status: "Completed")
+          - task_list.with_item(title: "Funding", hint: "The funds will be announced on 1 April 2022") do |item|
+            - item.with_status(text: "Cannot start yet", classes: "govuk-task-list__status--cannot-start-yet")
+      SNIPPET
+    end
+
     def task_list_with_coloured_tags
       <<~SNIPPET
-        = govuk_task_list do |task_list|
-          - task_list.with_item(title: "Design", status: govuk_tag(text: "Incomplete", colour: "green"))
-          - task_list.with_item(title: "Prototype", status: govuk_tag(text: "Incomplete", colour: "blue"))
-          - task_list.with_item(title: "Implementation", status: govuk_tag(text: "Incomplete", colour: "light-blue"))
-          - task_list.with_item(title: "User acceptance testing", status: govuk_tag(text: "Incomplete", colour: "red"))
-          - task_list.with_item(title: "Handover") do |item|
+        = govuk_task_list(id_prefix: "coloured-tags-example") do |task_list|
+          - task_list.with_item(title: "Design", href: "#", status: govuk_tag(text: "Green", colour: "green"))
+          - task_list.with_item(title: "Prototype", href: "#", status: govuk_tag(text: "Blue", colour: "blue"))
+          - task_list.with_item(title: "Implementation", href: "#", status: govuk_tag(text: "Light blue", colour: "light-blue"))
+          - task_list.with_item(title: "User acceptance testing", href: "#", status: govuk_tag(text: "Red", colour: "red"))
+          - task_list.with_item(title: "Handover", href: "#") do |item|
             - item.with_status do
-              = govuk_tag(text: "Incomplete", colour: "turquoise")
+              = govuk_tag(text: "Turquoise", colour: "turquoise")
       SNIPPET
     end
 
     def task_list_with_hints
       <<~SNIPPET
-        = govuk_task_list do |task_list|
-          - task_list.with_item do |item|
-            - item.with_title(text: "Check your qualifications", hint: "You need GCSEs in English and maths", href: "#")
-            - item.with_status(text: govuk_tag(text: "Done", colour: "green"))
+        = govuk_task_list(id_prefix: "task-list-with-hints") do |task_list|
+          - task_list.with_item(title: "Check your qualifications", hint: "You need GCSEs in English and maths", href: "#", status: govuk_tag(text: "Done", colour: "green"))
 
           - task_list.with_item do |item|
             - item.with_title(text: "Understand funding", hint: "Teacher training course fees are around £9,250 per year", href: "#")
