b71c4daaa43680ee433dc32c32dd4f29e32a1f58
Frankie Roberto <frankie@frankieroberto.com>
2023-11-27 12:01:41 +0000

    Update to use new combined logo svg

--- guide/lib/examples/header_helpers.rb
+++ guide/lib/examples/header_helpers.rb
@@ -2,34 +2,36 @@ module Examples
   module HeaderHelpers
     def header_normal
       <<~HEADER
-        = govuk_header
+        = govuk_header(homepage_url: "https://www.gov.uk")
+      HEADER
+    end
+
+    def header_with_service_name
+      <<~HEADER
+        = govuk_header(homepage_url: "https://www.gov.uk", service_name: "Apply for a juggling licence", service_url: "#")
       HEADER
     end
 
     def header_with_custom_logo_and_service_name
       <<~HEADER
-        = govuk_header(service_name: "Apply for a juggling licence") do |header|
+        = govuk_header(homepage_url: "#", service_name: "Apply for a juggling licence", service_url: "#") do |header|
           - header.with_custom_logo
-            | 🤹 GOV.UK
-      HEADER
-    end
-
-    def header_with_custom_logotype_and_homepage_url
-      <<~HEADER
-        = govuk_header(logotype: "Custom", homepage_url: "#")
+            svg focusable="false" role="img" class="app-header__logotype" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 30" height="30" width="40"
+              path d="M18,4 L36,30 0,30 18,4"
+            span.app-header__logotext MyOrg
       HEADER
     end
 
     def header_with_custom_product_name
       <<~HEADER
-        = govuk_header do |header|
+        = govuk_header(homepage_url: "#") do |header|
           - header.with_product_name(name: "Product")
       HEADER
     end
 
     def header_with_navigation_items
       <<~HEADER
-        = govuk_header(service_name: "Apply for a juggling licence") do |header|
+        = govuk_header(homepage_url: "https://www.gov.uk", service_name: "Apply for a juggling licence", service_url: "#") do |header|
           - header.with_navigation_item(text: "Amateur juggling", href: "#", active: false)
           - header.with_navigation_item(text: "Professional juggling", href: "#", active: false)
           - header.with_navigation_item(text: "Dangerous juggling", href: "#", active: false)
